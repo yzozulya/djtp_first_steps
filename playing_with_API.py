368c5b69190c34ad0a64ae2db132a97d003784dc
Yulia.Zozulya <yulia.zozulya@jetbrains.com>
2015-04-09 14:54:47 +0200

    Django Introduction Tutorial: Part 1 & Part 2

---
+++ playing_with_API.py
@@ -0,0 +1,83 @@
+from polls.models import Question, Choice  # Import the model classes we just wrote.
+
+
+Question.objects.all()
+
+# Create a new Question.
+# Support for time zones is enabled in the default settings file, so
+# Django expects a datetime with tzinfo for pub_date. Use timezone.now()
+# instead of datetime.datetime.now() and it will do the right thing.
+from django.utils import timezone
+q = Question(question_text="What's new?", pub_date=timezone.now())
+
+# Save the object into the database. You have to call save() explicitly.
+q.save()
+
+# Now it has an ID. Note that this might say "1L" instead of "1", depending
+# on which database you're using. That's no biggie; it just means your
+# database backend prefers to return integers as Python long integer
+# objects.
+print(q.id)
+
+
+# Access model field values via Python attributes.
+print(q.question_text)
+print(q.pub_date)
+
+# Change values by changing the attributes, then calling save().
+q.question_text = "What's up?"
+q.save()
+
+Question.objects.all()
+Question.objects.filter(id=1)
+Question.objects.filter(question_text__startswith='What')
+
+# Get the question that was published this year.
+from django.utils import timezone
+current_year = timezone.now().year
+Question.objects.get(pub_date__year=current_year)
+
+# Request an ID that doesn't exist, this will raise an exception.
+Question.objects.get(id=2)
+
+# Lookup by a primary key is the most common case, so Django provides a
+# shortcut for primary-key exact lookups.
+# The following is identical to Question.objects.get(id=1).
+Question.objects.get(pk=1)
+
+# Make sure our custom method worked.
+q = Question.objects.get(pk=1)
+q.was_published_recently()
+
+# Give the Question a couple of Choices. The create call constructs a new
+# Choice object, does the INSERT statement, adds the choice to the set
+# of available choices and returns the new Choice object. Django creates
+# a set to hold the "other side" of a ForeignKey relation
+# (e.g. a question's choice) which can be accessed via the API.
+q = Question.objects.get(pk=1)
+
+# Display any choices from the related object set -- none so far.
+q.choice_set.all()
+
+# Create three choices.
+q.choice_set.create(choice_text='Not much', votes=0)
+q.choice_set.create(choice_text='The sky', votes=0)
+c = q.choice_set.create(choice_text='Just hacking again', votes=0)
+
+# Choice objects have API access to their related Question objects.
+print(c.question)
+
+# And vice versa: Question objects get access to Choice objects.
+q.choice_set.all()
+q.choice_set.count()
+
+# The API automatically follows relationships as far as you need.
+# Use double underscores to separate relationships.
+# This works as many levels deep as you want; there's no limit.
+# Find all Choices for any question whose pub_date is in this year
+# (reusing the 'current_year' variable we created above).
+Choice.objects.filter(question__pub_date__year=current_year)
+
+# Let's delete one of the choices. Use delete() for that.
+c = q.choice_set.filter(choice_text__startswith='Just hacking')
+c.delete()
---
+++ polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_puplished_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_puplished_recently.admin_order_field = 'pub_date'
+    was_puplished_recently.boolean = True
+    was_puplished_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_test = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_test
