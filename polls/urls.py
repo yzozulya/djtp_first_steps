736e14c17ecdf42963b937d0c9d084e2b65d262d
Yulia.Zozulya <yulia.zozulya@jetbrains.com>
2015-04-09 15:59:14 +0200

    Part 5: more testing

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.DetailsView.as_view(), name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
