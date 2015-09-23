1b4e5995af7ea2a40106242206b23aee1b573f58
yzozulya <yuzozulya@gmail.com>
2015-09-23 14:47:09 +0300

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -20,5 +20,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
